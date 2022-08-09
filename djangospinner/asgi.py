e2d677b4738ca92f1f940a8ca61b100f149432f2
andywtse <atse4995@email.com>
2022-08-09 09:32:39 -0400

    Changes name

--- djangospinner/asgi.py
+++ djangospinner/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for moviespinner project.
+ASGI config for djangospinner project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moviespinner.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangospinner.settings')
 
 application = get_asgi_application()
--- djangospinner/wsgi.py
+++ djangospinner/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for moviespinner project.
+WSGI config for djangospinner project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moviespinner.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangospinner.settings')
 
 application = get_wsgi_application()
