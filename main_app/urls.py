837b4930cc68e68b93a71b569ccf125c4d7052cc
andywtse <atse4995@email.com>
2022-08-09 14:43:36 -0400

    Adds delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('collections/', views.collections_index, name='collections_index'),
   path('collections/<int:collection_id>/', views.collections_detail, name='collections_detail'),
+  path('collections/<int:collection_id>/add_content/', views.add_content, name='add_content'),
+  path('collections/<int:collection_id>/delete_content/<int:content_id>/', views.delete_content, name='delete_content'),
+  path('collections/create/', views.CollectionCreate.as_view(), name='collections_create'),
+  path('collections/<int:pk>/delete/', views.CollectionDelete.as_view(), name='collections_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from typing import Collection
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
@@ -6,7 +7,8 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import ContentCollection
+from .models import ContentCollection, Content
+from .forms import ContentForm
 
 def home(request):
   return render(request, 'home.html')
@@ -17,4 +19,31 @@ def collections_index(request):
 
 def collections_detail(request, collection_id):
   collection = ContentCollection.objects.get(id=collection_id)
-  return render(request, 'collections/detail.html', { 'collection': collection })
+  content_form = ContentForm()
+  return render(request, 'collections/detail.html', { 'collection': collection, 'content_form': content_form })
+
+def add_content(request, collection_id):
+  form = ContentForm(request.POST)
+  if form.is_valid():
+    new_content= form.save(commit=False)
+    new_content.collection_id = collection_id
+    new_content.save()
+  return redirect('collections_detail', collection_id=collection_id)
+
+def delete_content(request, collection_id, content_id):
+  content = Content.objects.get(id=content_id)
+  content.delete()
+  return redirect('collections_detail', collection_id=collection_id)
+
+class CollectionCreate(CreateView):
+  model = ContentCollection
+  fields = ['name', 'description']
+  success_url = '/collections/'
+  
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+  
+class CollectionDelete(DeleteView):
+  model = ContentCollection
+  success_url = '/collections/'
